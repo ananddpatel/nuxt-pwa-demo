78766439e5ce9d6dd9b3ea0f6c37895f3da62a28
Anand Patel <anandpatel3@hotmail.com>
2018-04-03 23:45:42 -0400

    added nav and footer to layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,13 +1,24 @@
 <template>
   <div>
+    <Nav/>
     <nuxt/>
+    <Footer/>
   </div>
 </template>
 
+<script>
+import Nav from "~/components/Nav.vue";
+import Footer from "~/components/Footer.vue";
+export default {
+  components: { Nav, Footer }
+};
+</script>
+
+
 <style>
-html
-{
-  font-family: "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
+html {
+  font-family: "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI",
+    Roboto, "Helvetica Neue", Arial, sans-serif;
   font-size: 16px;
   word-spacing: 1px;
   -ms-text-size-adjust: 100%;
@@ -16,13 +27,13 @@ html
   -webkit-font-smoothing: antialiased;
   box-sizing: border-box;
 }
-*, *:before, *:after
-{
+*,
+*:before,
+*:after {
   box-sizing: border-box;
   margin: 0;
 }
-.button--green
-{
+.button--green {
   display: inline-block;
   border-radius: 4px;
   border: 1px solid #3b8070;
@@ -30,13 +41,11 @@ html
   text-decoration: none;
   padding: 10px 30px;
 }
-.button--green:hover
-{
+.button--green:hover {
   color: #fff;
   background-color: #3b8070;
 }
-.button--grey
-{
+.button--grey {
   display: inline-block;
   border-radius: 4px;
   border: 1px solid #35495e;
@@ -45,8 +54,7 @@ html
   padding: 10px 30px;
   margin-left: 15px;
 }
-.button--grey:hover
-{
+.button--grey:hover {
   color: #fff;
   background-color: #35495e;
 }
